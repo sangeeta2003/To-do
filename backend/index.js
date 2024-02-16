a78c53975f565a2b0d33af09005b8b270fa424bd
*pknowledge* <mishrasangeeta944@gmail.com>
2024-02-16 11:20:17 +0530

    backend part

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,32 @@
 // with express josn middleware
 const express = require('express')
+
+const {createTodo,updateTodo} = require('./type')
 const app = express()
 
 app.use(express.json())
 
 app.get('/todo',function(req,res){
 
+
 })
 app.post('/todos',function(req,res){
-
+    const createPayload = req.body()
+const parsePayload = createTodo.safeParse(createPayload)
+if(!parsePayload.success){
+    res.status(411).json({
+        msg:"you sent the wrong input"
+    })
+    return;
+}
 })
 app.put('/completed',function(req,res){
-
+    const updatePayload = req.body()
+    const parsePayload = updateTodo.safeParse(updatePayload)
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg:"you sent the wrong input"
+        })
+        return;
+    }
 })
